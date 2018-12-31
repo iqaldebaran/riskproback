ee4aaafcef400421ee9212b1edce2cd30f72dc8e
Adolfo Vela <vela@tekplanner.com>
2018-12-30 22:07:04 -0600

    Prueba de cors en heroku

--- app.js
+++ app.js
@@ -30,6 +30,15 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
+//Prueba correccion en heroku cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 // Middleware Setup
 app.use(logger('dev'));
 app.use(bodyParser.json());
