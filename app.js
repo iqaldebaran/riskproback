235eba5bb3eb59311a328508b611cd0a69ac72ef
Adolfo Vela <vela@tekplanner.com>
2019-01-05 17:17:06 -0600

    Elimia parrafo

--- app.js
+++ app.js
@@ -30,14 +30,6 @@ const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.
 
 const app = express();
 
-//Prueba correccion en heroku cors - No funciono
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", '*');
-//   res.header("Access-Control-Allow-Credentials", true);
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-//   next();
-// });
 
 // Middleware Setup
 app.use(logger('dev'));
